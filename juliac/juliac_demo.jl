1d15f89a226126ed852714ee7c56fc0e3d791596
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2024-11-13 09:57:44 +0100

    up

--- juliac/juliac_demo.jl
+++ juliac/juliac_demo.jl
@@ -17,6 +17,9 @@ const lb = SA[0.1, 0.1, 50, 50, 5, -8500]
 const ub = SA[2, 2, 142, 140, 100, 0.0]
 measurement(x,u,p,t) = x # We can measure the full state
 const discrete_dynamics = LowLevelParticleFilters.rk4(dynamics, Ts)
+function discrete_dynamics_wrapper(x,u,p,t) # The silly identity function avoids a segfault in has_ip
+    return discrete_dynamics(x,u,p,t)
+end
 discrete_dynamics(x0, u0, p, 0.0)
 
 
@@ -27,14 +30,14 @@ const nx = 4 # Dimension of state
 const nu = 2 # Dimension of input
 const ny = 4 # Dimension of measurements
 
-const R1 = SMatrix{nx,nx}(Diagonal(typical_magnitudes .^ 2 ./ 1000))
+const R1 = 0.1*SA[0.0005790557598954602 -6.106518199161526e-5 -0.04490477069514286 -0.012974208985770341; -6.106518199161526e-5 0.000118640098523924 0.01151089963430807 0.003243003007406098; -0.04490477069514286 0.01151089963430807 7.705441811603613 1.8508801117780993; -0.012974208985770341 0.003243003007406098 1.8508801117780993 6.122836707225409]
 const R2 = SMatrix{nx,nx}(Diagonal(typical_magnitudes .^ 2 ./ 10))
 
 const d0 = LowLevelParticleFilters.SimpleMvNormal(x0,R1)   # Initial state Distribution
 
 println(Core.stdout, 1)
 # const kf   = KalmanFilter(_A, _B, _C, 0, R1, R2, d0, check=false)
-const kf = UnscentedKalmanFilter((x,u,p,t)->discrete_dynamics(x,u,p,t), measurement, R1, R2, d0; ny, nu, p=nothing) # The silly identity function avoids a segfault in has_ip
+const kf = UnscentedKalmanFilter(discrete_dynamics_wrapper, measurement, R1, R2, d0; ny, nu, p=nothing) 
 println(Core.stdout, 2)
 
 Base.@ccallable function main()::Cint
@@ -62,3 +65,7 @@ end
 # run(`./juliac_demo`)
 
 
+# using Plots
+# plot(sol, size=(1900,1200), ploty=false)
+# plot!(reduce(hcat, y)', sp=(1:4)', label="y")
+# plot!(reduce(hcat, xT)', sp=(1:4)', label="smooth")
